dd8091b8710f3a285abfe84b40a5880e1cd985c9
loinguyenanfin <loi.nguyen@anfin.vn>
2023-12-13 08:54:17 +0700

    refactor code

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var addr = flag.String("addr", "localhost:8080", "http service address")
+var addr = flag.String("addr", ":8089", "http service address")
 
 var upgrader = websocket.Upgrader{} // use default options
 
